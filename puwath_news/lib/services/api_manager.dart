31a9af1c53dc520cf252f3da694d42d6beff9bb2
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-05-19 16:25:23 +0530

    c1

--- puwath_news/lib/services/api_manager.dart
+++ puwath_news/lib/services/api_manager.dart
@@ -1,8 +1,13 @@
 import 'package:http/http.dart' as http;
+import 'package:puwath_news/constants/strings.dart';
 
 class API_Manager {
-  void getNews() {
-    var client = https.client();
-    client.get('Strings.news_url');
+  void getNews() async {
+    var client = http.Client();
+    var response = await client.get(Strings.news_url);
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+      
+    }
   }
 }
