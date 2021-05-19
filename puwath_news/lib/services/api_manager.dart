3b4bc4c92ffce6483dea0a99da552907179a8148
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-05-19 17:23:47 +0530

    c

--- puwath_news/lib/services/api_manager.dart
+++ puwath_news/lib/services/api_manager.dart
@@ -1,13 +1,25 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:puwath_news/constants/strings.dart';
+import 'package:puwath_news/models/newsinfo.dart';
 
 class API_Manager {
-  void getNews() async {
+  Future<NewsModel> getNews() async {
     var client = http.Client();
-    var response = await client.get(Strings.news_url);
-    if (response.statusCode == 200) {
-      var jsonString = response.body;
+    var newsModel = null;
 
+    try {
+      var response = await client.get(Strings.news_url);
+      if (response.statusCode == 200) {
+        var jsonString = response.body;
+        var jsonMap = json.decode(jsonString);
+
+        newsModel = NewsModel.fromJson(jsonMap);
+      }
+    } catch (Exception) {
+      return newsModel;
     }
+    return newsModel;
   }
 }
